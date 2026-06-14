f845ef0a70f86ffa54acf583a41433b731be84c9
boitymol <161038851+boitymol@users.noreply.github.com>
2026-06-14 17:57:40 +0200

    went over the messages class
    checked if everything was running

--- src/main/java/com/mycompany/quickchatapp/QuickChatApp.java
+++ src/main/java/com/mycompany/quickchatapp/QuickChatApp.java
@@ -215,176 +215,19 @@ public class QuickChatApp {
             System.out.println("Username or password incorrect,please try again,");
             
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-         //Scanner object to allow user input
-           Scanner input = new Scanner (System.in);
-           Messages messages = new Messages ();
-           
-          
-           //login object to allow input validation
-           Login login = new Login();
-           
-          //Declare variables
-      String firstName, lastName, Username, Password, cellPhonenum;
-      
-      //Prompt user to enter their details 
-        System.out.print("Enter your first name:");
-      firstName = input.nextLine();
-      
-      System.out.print("Enter your last name:");
-      lastName = input.nextLine();
-      
-      System.out.print("Enter a valid username:");
-      Username = input.nextLine();
-          
-          //Validate
-          
-          if(login.checkuserName(Username)){
-              System.out.println("Username successfully captured");
-          }else {
-              System.out.println("Username is not correctly formatted; please ensure that your usrename contains an underscore and is no more than five characters long");
-          }
-      
-      //Prompt user to enter their details 
-        System.out.print("Enter your password:");
-      Password = input.nextLine();
-      
-          //Validate
-          
-       if(!login.checkPasswordComplexity(Password)){
-          System.out.println("Pssword successfully captured"); 
-       }else{
-           System.out.print("Password is not correctly formatted, please ensure that the password contains atleast eigth characters, a capital letter, a number, and a special character");
-       }
-      
-      System.out.println("Enter your cellphone number:");
-      cellPhonenum = input.nextLine();
-      
-         //Validate
-         if(login.checkcellPhonenum(cellPhonenum)){
-                System.out.print("Cellphone number successfully added");
-         } else{
-                System.out.print("Cellphone not correctly formatted or does not contain international code");
-         }
-         
-     //display the registration status
-     System.out.println(login.registerUser(firstName, lastName, Username, Password, cellPhonenum));
-     
-     //declare temporary variables to allow user to login
-     String loginuserName, loginPassword;
-     
-      System.out.println("************LOGIN*************");
-      
-       System.out.println("Please enter your login login username:");
-       loginuserName = input.nextLine();
-        System.out.println("PLease enter your login password:");
-        loginPassword = input.nextLine();
-        
-         System.out.println(login.returnLoginStatus(firstName, lastName, Username, Password, loginuserName, loginPassword));
-         */
-                 
-         
-         //declare variable to store option
-         int option = 0;
-         int numMess = 0;
-         String recipient = ""; 
-         String messageHash;
-         String sentMessage;
-         String printMessages; 
-         String messageID;
-         String messageText;
-         
-         
-         
-         
-         
-         
-         //while loop to loop menu until user selects quit
-         do{
-         System.out.println("================= MENU =================");
-         System.out.println("1.SEND MESSAGES");
-         System.out.println("2.VIEW RECENTLY SENT MESSAGES");
-         System.out.println("3.QUIT");
-         System.out.print("Please select one option:");
-         option = Integer.parseInt(input.nextLine());
-         
-         
-         //create menu
-        switch(option){
-            case 1: 
-                //send messages
-                System.out.println("============SEND MESSAGES============");
-                
-                //prompt user to enter set of numbers
-                System.out.print("How many messages do you want to send?");
-                numMess = Integer.parseInt(input.nextLine());
-                
-                //Loop numMessages times
-                for(int i = 0; i < numMess; i++){
-                    
-                    System.out.println("\nMessage " + (i + 1) + " of " + numMess);
-                    
-                    //2. Get recipient with validation
-                    while(!message.checkcellPhonenum(recipient)){
-                        System.out.print("Enter recipient cellphone number with international code:");
-                        recipient = input.nextLine();
-                        System.out.println(message.checkRecipientCell(recipient));
-                    }
-                    
-                    recipient = "";
-                    
-                }
-                System.out.println("Enter your message:");
-                messageText = input.nextLine();
-                
-                while (!message.checkMessageLength(messageText)){
-                    System.out.println("Message must be 250 character or less!!");
-                    System.out.println("Enter message:");
-                    messageText = input.nextLine();
-                    
-                }
-                
-                messageID = message.generateMessageID();
-                messageHash = message.createMessageHash(messageID ,messageText);
-                
-                 
-                break; 
-                
-            case 2:
-                //show recently sent messages
-                System.out.println("Coming Soon!");
-                break;
-                
-                
-            case 3:
-                //quit
-                System.out.println("Goodbye!!");
-                break;
-                
-            default:
-                //if user has selected an invalid option
-                System.out.println("Invalid option, please enter 1,2 or 3");
-                break;
-                
-        }
-        
-        
-         }while(option !=3);
-         
-         
     }
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
